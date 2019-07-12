673d70cc58a8cb427f033b0eef7d9337bcde9f7b
Nathan Singleton <nathan.singleton@peoplekeep.com>
2019-07-12 01:05:23 -0600

    Making cli tool for better ease of use and argument handling

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Infinite house of pancakes
 
-To run the project, clone the repository and then run `npm run flip` and pass a series of pancake strings (eg +-).
+This is packaged as a simple cli tool. To run the project:
+
+1. clone the repository
+2. `npm install -g`
+3. Run the `pancakes` command and pass a series of pancake strings (eg +- +-+-+-)
+
+You can get more verbose output that will show the series of moves made by passing `--verbose` as an argument.
 
 ```bash
-$ npm run flip -- +- --+- +-+-+-
-```
-
-You can also get more verbose output that will show the series of moves made by passing true as the final input.
-
-```bash
-$ npm run flip +-- -++ +-+-+- true
+$ pancakes +-- -++ +-+-+- --verbose
 ```
 
 To run the jest tests:
--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
+#!/usr/bin/env node
 const flipPancakes = require('./flip-pancakes')
 const chalk = require('chalk')
+const VERBOSE_FLAG = '--verbose'
 // list of strings from command line
-let stacks = process.argv.slice(2)
-let verbose = false
+let stacks = process.argv.slice(2).filter(str => str !== VERBOSE_FLAG)
+// check for verbose mode
+let verbose = process.argv.indexOf(VERBOSE_FLAG) > -1
 
-if (stacks[stacks.length - 1] === 'true') {
-  verbose = true
-  stacks.pop()
-}
-
-console.log(chalk.cyan.bold('Revenge of the Pancakes'))
+// log out common program info
+console.log(chalk.cyan.bold('\nRevenge of the Pancakes'))
 console.log(chalk.cyan('---------------------------'))
 console.log(chalk.white(`Test Cases: ${stacks.length}\n`))
 
+// verbose output (if requested)
 if (verbose) {
   stacks.forEach((str, i) => {
     const { moves, count } = flipPancakes(str)
+    // header
     console.log(
-      chalk.yellow(`Case #${i + 1}: ${count} move${count > 1 ? 's' : ''}`),
+      chalk.yellow(`Case #${i + 1}: ${count} move${count === 1 ? '' : 's'}`),
     )
+    // make sure to log starting string
     console.log(chalk.green(`Starting value: ${str}`))
+    // a table stepping through the sequence of moves
     console.table(
+      // convert to an object of objects to get nice labels in the console.table output
       moves.reduce(
         (obj, str, i) => ({
           ...obj,
@@ -29,10 +33,13 @@ if (verbose) {
         {},
       ),
     )
+    // new line for nice spacing between tables
     console.log('\n')
   })
 } else {
+  // standard output
   console.table(
+    // convert to an object of objects to get nice labels in the console.table output
     stacks.reduce((obj, str, i) => {
       const { moves, count } = flipPancakes(str)
       return {
@@ -44,4 +51,6 @@ if (verbose) {
       }
     }, {}),
   )
+  // new line for nice spacing between runs
+  console.log('\n')
 }
